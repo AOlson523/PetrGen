513359a8f4f3a467dfa6f6a6e58e17b66d95fdf3
Anderson Olson <92558834+AOlson523@users.noreply.github.com>
2023-02-03 22:43:04 -0800

    Update webScraper.py

--- Backend/webScraper.py
+++ Backend/webScraper.py
@@ -1,1 +1,13 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup as bs
 
+def urlExtract(url):
+    response = requests.get(url)
+
+
+    soup = bs(response.content, 'html.parser')
+
+    img_div = soup.findAll("div",attrs={"class","_aagv"})
+    for i in img_div:
+        img_url = i.findAll("img", attrs="src")
