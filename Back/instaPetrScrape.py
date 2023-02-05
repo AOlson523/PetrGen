e7e3226e94f08faa3dcc1308e77d7858c011e8ca
Anderson Olson <92558834+AOlson523@users.noreply.github.com>
2023-02-04 17:38:46 -0800

    Update instaPetrScrape.py

--- Back/instaPetrScrape.py
+++ Back/instaPetrScrape.py
@@ -1,1 +1,26 @@
+# check if a file is a jpg 
 
+import os
+from PIL import Image 
+import PIL 
+
+def isJPG(fileName_caption, directory):
+    for i, filename in enumerate(os.listdir(directory)):
+        d = dict()
+        if filename[-3:] == "jpg":
+            d["image"] = Image.open(directory+"/"+filename)
+            d["caption"] = ""
+            if i + 2 < len(os.listdir(directory)) and os.listdir(directory)[i + 2][-3:] == "txt":
+                with open(directory+"/"+os.listdir(directory)[i + 2], 'r', encoding='utf8') as txtFile:
+                    d["caption"] = " ".join(txtFile.readlines()).strip("\n")
+            fileName_caption.append(d)
+
+
+def main():
+    fileName_caption = list()
+    path = "C:/Users/junya/Downloads/UCI/HackAtUCI/PetrGen/Backend/"
+    for filename in os.listdir(path):
+        if os.path.isdir(path +filename):
+            isJPG(fileName_caption, path+filename)
+
+main()
