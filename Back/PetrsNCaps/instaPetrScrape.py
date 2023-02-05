0ca2a56616cf0a47a6b864f7cffae5172bf53b95
junyanw3 <junyanwu3@gmail.com>
2023-02-04 19:30:53 -0800

    update instaPetrScrape.py

--- Back/PetrsNCaps/instaPetrScrape.py
+++ Back/PetrsNCaps/instaPetrScrape.py
@@ -4,33 +4,59 @@ import PIL
 import json 
 
 
-def isJPG(fileName_caption, directory):
-    for i, filename in enumerate(os.listdir(directory)):
-        d = dict()
+def loopPetrDir(fileName_caption, directory):
+    """
+    This function will loop though the petr directory of images and text files.
+    It will add create a dictionary of {image: imageName, caption: text} and add to the list. 
+    """
+    for filename in os.listdir(directory):
+        d = dict() # {image: imageName, caption: text}
         if filename[-3:] == "jpg":
-            d["image"] = filename # Image.open(directory+"/"+filename)
+            d["image"] = filename
             d["caption"] = ""
-            if i + 2 < len(os.listdir(directory)) and os.listdir(directory)[i + 2][-3:] == "txt":
-                with open(directory+"/"+os.listdir(directory)[i + 2], 'r', encoding='utf8') as txtFile:
+
+            # if there are multiple images per post,
+            # the filename will end in _n for which n is the number of images in a post
+            # we will rename the filename, by removing the _n
+            if filename[-5].isnumeric(): 
+                filename = filename[0:-6] + ".jpg"
+
+            # if the caption file exists in the directory 
+            # we will open the text file and 
+            # set the contents of the text file as the caption 
+            if filename[:-3]+"txt" in os.listdir(directory):
+                with open(directory+"/"+filename[:-3]+"txt", 'r', encoding='utf8') as txtFile:
                     d["caption"] = " ".join(txtFile.readlines()).strip("\n")
+            
+            # add the newly created dictionary to the list 
             fileName_caption.append(d)
 
 
-def main(path):
+def loopDir(path):
+    """
+    This function will iterate though the folder to look up directories of petr folders.
+    And will add {images and captions} to the returned list.
+    """
     fileName_caption = list()
     for filename in os.listdir(path):
+        # if the filename is a petr director, add to the list 
         if os.path.isdir(path+"/"+filename):
-            isJPG(fileName_caption, path+"/"+filename)
+            loopPetrDir(fileName_caption, path+"/"+filename)
     return fileName_caption
 
 
-if __name__ == "__main__":
+def main():
+    """
+    This is the main function, which will ask the user for their path, 
+    loop through the directory, and create a json file from the data. 
+    """
     path = input("Enter path to directory: ")
-    jsonData = main(path)
-    print(jsonData)
-    print(len(jsonData))
+    jsonData = loopDir(path)
 
+    # will create a data.json file to write the list contents in json format 
     with open("data.json", "w") as f:
         json.dump(jsonData, f)
-    
-# C:\Users\junya\Downloads\UCI\HackAtUCI\PetrGen\Back\PetrsNCaps
+
+
+if __name__ == "__main__":
+    main()
