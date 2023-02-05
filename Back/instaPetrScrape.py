8d534db06239f13a546884341ab689b8d5218779
junyanw3 <67295222+junyanw3@users.noreply.github.com>
2023-02-04 17:57:20 -0800

    Update instaPetrScrape.py

--- Back/instaPetrScrape.py
+++ Back/instaPetrScrape.py
@@ -1,14 +1,14 @@
-# check if a file is a jpg 
-
 import os
 from PIL import Image 
 import PIL 
+import json 
+
 
 def isJPG(fileName_caption, directory):
     for i, filename in enumerate(os.listdir(directory)):
         d = dict()
         if filename[-3:] == "jpg":
-            d["image"] = Image.open(directory+"/"+filename)
+            d["image"] = (directory+"/"+filename) # Image.open(directory+"/"+filename)
             d["caption"] = ""
             if i + 2 < len(os.listdir(directory)) and os.listdir(directory)[i + 2][-3:] == "txt":
                 with open(directory+"/"+os.listdir(directory)[i + 2], 'r', encoding='utf8') as txtFile:
@@ -16,11 +16,15 @@ def isJPG(fileName_caption, directory):
             fileName_caption.append(d)
 
 
-def main():
+def main(path):
     fileName_caption = list()
-    path = "C:/Users/junya/Downloads/UCI/HackAtUCI/PetrGen/Backend/"
     for filename in os.listdir(path):
         if os.path.isdir(path +filename):
             isJPG(fileName_caption, path+filename)
+    return json.dumps(fileName_caption)
 
-main()
+
+if __name__ == "__main__":
+    path = input("Enter path to directory: ")
+    jsonData = main(path)
+    print(jsonData)
